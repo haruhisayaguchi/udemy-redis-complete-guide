47b508b5bfa4e1bb5628c3719dee3c068e73abd5
haruhisayaguchi <hal.yaguchi@gmail.com>
2026-02-22 17:56:16 +0900

    [modify] section_18

--- rbay/src/services/queries/bids.ts
+++ rbay/src/services/queries/bids.ts
@@ -1,13 +1,11 @@
 import { bidHistoryKey, itemsByPriceKey, itemsKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async (isolatedClient) => {
-		await isolatedClient.watch(itemsKey(attrs.itemId));
-
+	return withLock(attrs.itemId, async (signal: any) => {
 		const item = await getItem(attrs.itemId);
 
 		if (!item) {
@@ -22,20 +20,55 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 
 		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-		return isolatedClient
-			.multi()
-			.rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id), {
+		if (signal.expired) {
+			throw new Error("Lock expired, can't write any more data")
+		}
+
+		return Promise.all([
+			client.rPush(bidHistoryKey(attrs.itemId), serialized),
+			client.hSet(itemsKey(item.id), {
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId
-			})
-			.zAdd(itemsByPriceKey(), {
+			}),
+			client.zAdd(itemsByPriceKey(), {
 				value: item.id,
 				score: attrs.amount
 			})
-			.exec();
-	});
+		])
+	})
+
+	// return client.executeIsolated(async (isolatedClient) => {
+	// 	await isolatedClient.watch(itemsKey(attrs.itemId));
+
+	// 	const item = await getItem(attrs.itemId);
+
+	// 	if (!item) {
+	// 		throw new Error("Item does not exits");
+	// 	}
+	// 	if (item.price >= attrs.amount) {
+	// 		throw new Error("Bid too low");
+	// 	}
+	// 	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+	// 		throw new Error("Item closed to bidding");
+	// 	}
+
+	// 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+	// 	return isolatedClient
+	// 		.multi()
+	// 		.rPush(bidHistoryKey(attrs.itemId), serialized)
+	// 		.hSet(itemsKey(item.id), {
+	// 			bids: item.bids + 1,
+	// 			price: attrs.amount,
+	// 			highestBidUserId: attrs.userId
+	// 		})
+	// 		.zAdd(itemsByPriceKey(), {
+	// 			value: item.id,
+	// 			score: attrs.amount
+	// 		})
+	// 		.exec();
+	// });
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
--- rbay/src/services/redis/lock.ts
+++ rbay/src/services/redis/lock.ts
@@ -1,6 +1,39 @@
-export const withLock = async () => {};
+import { randomBytes } from "crypto";
+import { client } from "./client";
 
-const buildClientProxy = () => {};
+export const withLock = async (key: string, cb: (signal: any) => any) => {
+	// initialize a few variables to control retry behavior
+	const retryDelayMs = 100;
+	let retries = 20;
+
+	// generate a random value to store at the lock key
+	const token = randomBytes(6).toString("hex");
+	// create the lock key
+	const lockKey = `lock:${key}`;
+	// set up a while loop to implement the retry behavior
+	while (retries >= 0) {
+		retries--;
+		// try to do a set nx operation
+		const acquired = await client.set(lockKey, token, { NX: true, PX: 2000 });
+		if (!acquired) {
+			// else brief pause (retryDelays) and then retry
+			await pause(retryDelayMs);
+			continue;
+		}
+		// if the set is successful, then run the callback
+		try {
+			const signal = { expired: false };
+			setTimeout(() => { signal.expired = true }, 2000)
+			const result = await cb(signal);
+			return result;
+		} finally {
+			// unset the locked set
+			await client.unlock(lockKey, token);
+		}
+	}
+};
+
+const buildClientProxy = () => { };
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
